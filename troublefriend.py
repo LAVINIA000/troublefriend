ba8be72309d4959e6d9cba16f104432c72bd1069
LAVINIA000 <78450830+LAVINIA000@users.noreply.github.com>
2021-02-03 01:44:01 +0300

    Add files via upload

---
+++ troublefriend.py
@@ -0,0 +1,33 @@
+print ("Hello Man, What are you doing?\n1 Im a sad\n2 Im good im happy\n3 Im soo boredd")
+
+seç = int(input("Choice:"))
+
+if seç == 1:
+    print ("Uh don't worry man, nothing is more valuable than you know, let s play backgammon with you")
+    print ("And you know, i know your lover she is beatufil she wants to be dear with you")
+    print ("\n1 Im a sad\n2 Im good im happy\n3 Im soo boredd")
+###########################################################################################
+elif seç == 2:
+    print ("Its soo nice i like you have a nice day!")
+    print ("\n1 Im a sad\n2 Im good im happy\n3 Im soo boredd")
+###########################################################################################
+elif seç == 3:
+    print ("Uh its not good do you want to play cheese?\n1 Yes\n2 No i like some war games")
+
+seçiş = int(input("Choice:"))
+
+if seçiş == 1:
+    print ("Lets open chrome and type lichess.org and play chess!")
+
+elif seçiş == 2:
+    print ("Oh okay do you want to play battlefield 1?\n1 Yes i likes it\n2 No i like some puzzle games")
+#########################################################################################################
+seçenek = int(input("Choice="))
+
+if seçenek == 1:
+    print ("Yee boi lets open steam or epic games store and play it (:D)")
+
+elif seçenek == 2:
+    print ("Okay lets open steam and search it The room and lets play it!")
+else:
+    print ("Wrong command!")
